dba69d26cae1a03ecde13407cf452f085e2757cf
Charlotte Zhang <ksw6ux@virginia.edu>
2023-03-30 17:15:26 -0400

    start

--- src/main/java/edu/virginia/cs/gui/WordleController.java
+++ src/main/java/edu/virginia/cs/gui/WordleController.java
@@ -1,14 +1,19 @@
 package edu.virginia.cs.gui;
 
+import edu.virginia.cs.wordle.WordleImplementation;
+import edu.virginia.cs.wordle.Wordle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 
 public class WordleController {
+    private Wordle wordle = new WordleImplementation();
     @FXML
-    private Label welcomeText;
+    private GridPane grid = new GridPane();
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void createGrid() {
+
     }
 }
