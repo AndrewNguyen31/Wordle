014e807534436585075c92f9015228197def9039
Charlotte Zhang <ksw6ux@virginia.edu>
2023-03-30 21:32:04 -0400

    my part done

--- src/main/java/edu/virginia/cs/gui/WordleApplication.java
+++ src/main/java/edu/virginia/cs/gui/WordleApplication.java
@@ -3,6 +3,7 @@ package edu.virginia.cs.gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,7 @@ public class WordleApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(WordleApplication.class.getResource("wordle-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 300, 400);
         stage.setTitle("Welcome to Wordle!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/edu/virginia/cs/gui/WordleController.java
+++ src/main/java/edu/virginia/cs/gui/WordleController.java
@@ -1,19 +1,132 @@
 package edu.virginia.cs.gui;
 
+import edu.virginia.cs.wordle.IllegalWordException;
 import edu.virginia.cs.wordle.WordleImplementation;
 import edu.virginia.cs.wordle.Wordle;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class WordleController {
     private Wordle wordle = new WordleImplementation();
+    int row = 0;
+    int col = 0;
     @FXML
     private GridPane grid = new GridPane();
+    @FXML
+    private Label text;
+    @FXML
+    private Label realText;
+
+    public void initialize() {
+        createGrid();
+        setEvenHandler();
+    }
 
     private void createGrid() {
+        for (int row = 0; row < 6; row++) {
+            for (int col = 0; col < 5; col++) {
+                TextField text = new TextField();
+                text.setStyle("-fx-background-color: black; -fx-border-color: grey;-fx-text-fill:white;");
+                text.setPrefWidth(50);
+                text.setPrefHeight(400);
+                text.setFont(Font.font("verdana", FontWeight.BOLD, 20 ));
+                text.setTextFormatter(new TextFormatter<>((change) -> {
+                    change.setText(change.getText().toUpperCase());
+                    return change;
+                }));
+                text.setEditable(false);
+                if (row == 0 && col == 0) {
+                    text.setEditable(true);
+                    requestFocus(text);
+                }
+                grid.add(text, col, row);
+                Insets insets = new Insets(0,2.5,2.5,2.5);
+                grid.setMargin(text,insets);
+            }
+        }
+    }
 
+    private void setEvenHandler(){
+        grid.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                text.setText("");
+                KeyCode code = event.getCode();
+                TextField oldField = getTextField(col, row);
+                oldField.setEditable(false);
+                String word = getWord(row);
+                if (checkValidWord(word)) {
+                    moveForward();
+                }
+                else {
+                    if (code == KeyCode.BACK_SPACE) {
+                        moveBackward();
+                    } else if (col < 4) {
+                        moveForward();
+                    }
+                }
+                TextField newField = getTextField(col, row);
+                newField.setEditable(true);
+                requestFocus(newField);
+            }
+        });
+    }
+
+    private String getWord(int row) {
+        String word = "";
+        for (int col = 0; col < 5; col++) {
+            word += getTextField(col, row).getText();
+        }
+        return word;
+    }
+
+    private boolean checkValidWord(String word) {
+        try {
+            wordle.submitGuess(word);
+            return true;
+        }
+        catch (IllegalWordException e) {
+            if (word.length() == 5) {
+                text.setText("Invalid word! Please try again.");
+            }
+            return false;
+        }
+    }
+
+    private void requestFocus(TextField textField) {
+        Platform.runLater(() -> {
+            if (!textField.isFocused()) {
+                textField.requestFocus();
+                requestFocus(textField);
+            }
+        });
+    }
+
+    private TextField getTextField (int col, int row){
+        int index = col + row * 5;
+        return (TextField) grid.getChildren().get(index);
+    }
+
+    private void moveForward() {
+        col = (col+1) % 5;
+        if (col == 0) {
+            row = row + 1;
+        }
+    }
+
+    private void moveBackward() {
+        if (col > 0) col--;
     }
 }
