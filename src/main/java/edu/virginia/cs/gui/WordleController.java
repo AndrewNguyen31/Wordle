6c42475ae41e33187e09b6a82fe0574d2c29cda3
Andrew Nguyen-Tran <andrewnguyen1115@gmail.com>
2023-04-09 23:00:08 -0400

    - Finishing touches

--- src/main/java/edu/virginia/cs/gui/WordleController.java
+++ src/main/java/edu/virginia/cs/gui/WordleController.java
@@ -1,12 +1,9 @@
 package edu.virginia.cs.gui;
-
 import edu.virginia.cs.wordle.IllegalWordException;
 import edu.virginia.cs.wordle.LetterResult;
 import edu.virginia.cs.wordle.WordleImplementation;
 import edu.virginia.cs.wordle.Wordle;
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -20,13 +17,11 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
-
 import java.util.Optional;
-
 public class WordleController {
-    private Wordle wordle = new WordleImplementation();;
-    int row = 0, col = 0;
-
+    private Wordle wordle = new WordleImplementation();
+    int row = 0;
+    int col = 0;
     LetterResult[] letterColors;
     @FXML
     private GridPane grid = new GridPane();
@@ -47,18 +42,14 @@ public class WordleController {
                 text.setStyle("-fx-background-color: black; -fx-border-color: grey;-fx-text-fill:white;");
                 text.setPrefWidth(50);
                 text.setPrefHeight(400);
-                text.setFont(Font.font("verdana", FontWeight.BOLD, 20 ));
+                text.setFont(Font.font("verdana", FontWeight.BOLD, 20));
                 text.setTextFormatter(new TextFormatter<>((change) -> {
                     change.setText(change.getText().toUpperCase());
                     return change;
                 }));
-                text.textProperty().addListener(new ChangeListener<String>() {
-                    @Override
-                    public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                        if (text.getText().length() > 1){
-                            String str = text.getText().substring(0, 1);
-                            text.setText(str);
-                        }
+                text.textProperty().addListener((observable, oldValue, newValue) -> {
+                    if (newValue.length() > 1) {
+                        text.setText(String.valueOf(newValue.charAt(0)));
                     }
                 });
                 text.setEditable(false);
@@ -67,40 +58,53 @@ public class WordleController {
                     requestFocus(text);
                 }
                 grid.add(text, col, row);
-                Insets insets = new Insets(0,2.5,2.5,2.5);
-                grid.setMargin(text,insets);
+                Insets insets = new Insets(0, 2.5, 2.5, 2.5);
+                grid.setMargin(text, insets);
             }
         }
     }
 
-    private void setEventHandler(){
+    private void setEventHandler() {
         grid.setOnKeyReleased(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                if(wordle.isGameOver() || isInvalidKey(event)){return;}
-                text.setText("");
-                KeyCode code = event.getCode();
-                TextField oldField = getTextField(col, row);
-                oldField.setEditable(false);
+                if (wordle.isGameOver()) return;
+                else if (isCursor(event)) {
+                    event.consume();
+                    return;
+                }
                 String word = getWord(row);
                 if (checkValidWord(word)) {
-                    colorRow(word);
+                    if (colorRow(word)) return;
+                    text.setText("");
+                    makeRowUneditable(row);
                     moveForward();
+                } else {
+                    if (event.getCode() == KeyCode.BACK_SPACE) moveBackward();
+                    else if (col < 4) moveForward();
                 }
-                else {
-                    if (code == KeyCode.BACK_SPACE) {
-                        moveBackward();
-                    } else if (col < 4) {
-                        moveForward();
-                    }
-                }
-                if(!wordle.isGameOver()){
-                    TextField newField = getTextField(col, row);
-                    newField.setEditable(true);
-                    requestFocus(newField);
-                }
+                TextField newField = getTextField(col, row);
+                newField.setEditable(true);
+                requestFocus(newField);
             }
         });
+    }
+
+    private boolean isCursor(KeyEvent keyEvent) {
+        KeyCode code = keyEvent.getCode();
+        switch (code) {
+            case LEFT:
+            case RIGHT:
+            case UP:
+            case DOWN:
+            case PAGE_UP:
+            case PAGE_DOWN:
+            case HOME:
+            case END:
+            case TAB:
+                return true;
+        }
+        return false;
     }
 
     private String getWord(int row) {
@@ -115,8 +119,7 @@ public class WordleController {
         try {
             letterColors = wordle.submitGuess(word);
             return true;
-        }
-        catch (IllegalWordException e) {
+        } catch (IllegalWordException e) {
             if (word.length() == 5) {
                 text.setText("Invalid word! Please try again.");
             }
@@ -124,110 +127,68 @@ public class WordleController {
         }
     }
 
-    private boolean isInvalidKey(KeyEvent event){
-        switch(event.getCode()){
-            case LEFT:
-            case RIGHT:
-            case UP:
-            case DOWN:
-            case PAGE_UP:
-            case PAGE_DOWN:
-            case TAB:
-            case SHIFT:
-            case HOME:
-            case END:
-            case CONTROL:
-                return true;
-        }
-        return false;
-    }
-
     private void requestFocus(TextField textField) {
         Platform.runLater(() -> {
             if (!textField.isFocused()) {
                 textField.requestFocus();
-                requestFocus(textField);
             }
         });
     }
 
-    private TextField getTextField (int col, int row){
+    private TextField getTextField(int col, int row) {
         int index = col + row * 5;
         return (TextField) grid.getChildren().get(index);
     }
 
     private void moveForward() {
-        col = (col+1) % 5;
-        if (col == 0) {
-            row = row + 1;
-        }
+        col = (col + 1) % 5;
+        if (col == 0) row++;
     }
 
     private void moveBackward() {
         if (col > 0) col--;
     }
 
-    private void colorRow(String guess) {
+    private Boolean colorRow(String guess) {
         for (int col = 0; col < 5; col++) {
             TextField text = getTextField(col, row);
             text.setText(String.valueOf(guess.charAt(col)));
-            if(letterColors[col].equals(LetterResult.GRAY)) {text.setStyle("-fx-background-color: dimgray; -fx-border-color: grey;-fx-text-fill:white;");}
-            if(letterColors[col].equals(LetterResult.GREEN)) {text.setStyle("-fx-background-color: green; -fx-border-color: grey;-fx-text-fill:white;");}
-            if(letterColors[col].equals(LetterResult.YELLOW)) {text.setStyle("-fx-background-color: #F1C40F; -fx-border-color: grey;-fx-text-fill:white;");}
+            if (letterColors[col].equals(LetterResult.GRAY)) {
+                text.setStyle("-fx-background-color: dimgray; -fx-border-color: grey;-fx-text-fill:white;");
+            }
+            if (letterColors[col].equals(LetterResult.GREEN)) {
+                text.setStyle("-fx-background-color: green; -fx-border-color: grey;-fx-text-fill:white;");
+            }
+            if (letterColors[col].equals(LetterResult.YELLOW)) {
+                text.setStyle("-fx-background-color: #F1C40F; -fx-border-color: grey;-fx-text-fill:white;");
+            }
         }
-        if (!wordle.isGameOver()) return;
-
+        if (!wordle.isGameOver()) return false;
         if (wordle.isWin()) text.setText("Correct! You won!");
         else if (wordle.isLoss()) text.setText("Incorrect. Out of guesses. Correct word: " + wordle.getAnswer());
         playAgainAlert();
+        return true;
     }
 
-    private boolean isWin(String word) {
-        if(word.equals(wordle.getAnswer())){
-            text.setText("Correct! You won!");
-            for (int col = 0; col < 5; col++) {
-                TextField text = new TextField();
-                text.setText(String.valueOf(word.charAt(col)));
-                text.setStyle("-fx-background-color: green; -fx-border-color: grey;-fx-text-fill:white;");
-                text.setPrefWidth(50);
-                text.setPrefHeight(400);
-                text.setFont(Font.font("verdana", FontWeight.BOLD, 20 ));
-                text.setTextFormatter(new TextFormatter<>((change) -> {
-                    change.setText(change.getText().toUpperCase());
-                    return change;
-                }));
-                grid.add(text, col, row);
-                Insets insets = new Insets(0,2.5,2.5,2.5);
-                grid.setMargin(text,insets);
-            }
-
-            for (int row = 0; row < 6; row++) {
-                for (int col = 0; col < 5; col++) {
-                    TextField curField = getTextField(col, row);
-                    curField.setEditable(false);
-                }
-            }
-            return true;
+    private void makeRowUneditable(int r) {
+        for (int col = 0; col < 5; col++) {
+            TextField curField = getTextField(col, r);
+            curField.setEditable(false);
         }
-        else {return false;}
     }
 
-
-    private void playAgainAlert(){
+    private void playAgainAlert() {
         Alert alert = new Alert(Alert.AlertType.NONE,
                 "Play again?",
                 ButtonType.YES, ButtonType.NO);
-
         Optional<ButtonType> YesNo = alert.showAndWait();
-        if (YesNo.get() == ButtonType.YES){
+        if (YesNo.get() == ButtonType.YES) {
             wordle = new WordleImplementation();
-            resetGrid();
             row = 0;
             col = 0;
+            resetGrid();
+            setEventHandler();
             text.setText("");
-            TextField t = getTextField(col, row);
-            t.setEditable(true);
-            requestFocus(t);
         } else {
             Platform.exit();
         }
